cd4d1cc819b8128eb3d26f99cafe10692736b46e
Muhammet Kaya <66886314+kayamuhammet@users.noreply.github.com>
2025-04-16 22:30:15 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SimpleNotesApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
